0b4f2427de1ff3588c642e95eccdac7c6ef7ef84
ShubhamMhatre2727 <shubhammhatre2727@gmail.com>
2024-05-03 14:22:31 +0530

    added font color changer button and also done some modification in code

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import colorchooser
 import json
 import time
 import threading
@@ -15,7 +16,7 @@ class thread(threading.Thread):
         if(current_track):
             if current_track['currently_playing_type'] == "ad":
                 list_box.insert(0, "ad playing")
-                print("ad playing")
+                # print("ad playing")
                 time.sleep(30)
                 list_box.delete(0, "end")
                 self.run()
@@ -29,6 +30,8 @@ class thread(threading.Thread):
                     self.generate_lyrics(synced_lyrics, current_track)
                 else:
                     status.config(text="lyrics not found")
+        else:
+            start.config(state="normal")
     
     def generate_lyrics(self, synced_lyrics, current_track):
         try:
@@ -46,7 +49,7 @@ class thread(threading.Thread):
 
     def display_lyrics(self, i, list, prev, current_track):
         if(i<len(list)):
-            print(list[i][11:])
+            # print(list[i][11:])
             list_box.insert(i, list[i][11:])
             list_box.see("end")
             curr = float(list[i][1:3]) * 60 + float(list[i][4:6])
@@ -74,57 +77,58 @@ display = False
 def toggle():
     global display
     if display :
-        # btn1.grid()
         start.grid()
+        close.grid()
         toggle.grid(row=1, column=1)
         display = False
         toggle.config(text="hide", width=button_width)
+        color.config(width=button_width)
     else:
-        # btn1.grid_remove()
         start.grid_remove()
+        close.grid_remove()
         toggle.grid(row=0,column=1)
         display = True
         toggle.config(text="X", width=5)
+        color.config(width=5)
 
     root.overrideredirect(display)
 
 
+def change_color():
+    if list_box.cget("foreground") == "white":
+        list_box.config(fg="black")
+        color.config(text="⚪")
+    else: 
+        list_box.config(fg="white")
+        color.config(text="⚫")
 
 root = tk.Tk()
 root.title("spotify lyrics")
-
 # Set the window always on top
 root.wm_attributes("-topmost", True)
 # Make the window non-resizable
 root.resizable(width=False, height=False)
 
 # following line will make every bg transparent having color red
-hidden_color = "black"
+hidden_color = "gray"
 root.wm_attributes("-transparentcolor", hidden_color)
 
 frame = tk.Frame(root, bg=hidden_color)
 frame.pack()
 
 button_width = 12
-label_width = 40
-
-# btn1 = tk.Button(frame, text="generate token", width=button_width, command=generate_token)
 start = tk.Button(frame, text="start", width=button_width, command=start)
 toggle = tk.Button(frame, text="hide", width=button_width, command=toggle)
+close = tk.Button(frame, text="close", width=button_width, command=lambda:root.destroy())
+color = tk.Button(frame, text="⚫", width=button_width, command=change_color)
 
-status = tk.Label(frame, width=label_width, bg=hidden_color, fg="white")
-label1 = tk.Label(frame, text="label1", width=label_width, bg=hidden_color, fg="white")
-label2 = tk.Label(frame, text="label2", width=label_width, bg=hidden_color, fg="white")
-
-# btn1.grid(row=0, column=1)
+list_box = tk.Listbox(frame, bg=hidden_color,bd=0, font=("", 12, "bold"), justify="right" ,highlightthickness=0 , fg="white", width=50, height=(start.winfo_height()*3))
+list_box.insert(0, "sample text")
 start.grid(row=0, column=1)
 toggle.grid(row=1, column=1)
+close.grid(row=2, column=1)
+color.grid(row=3, column=1)
 
-# status.grid(row=0, column=0)
-# label1.grid(row=1, column=0, pady=5)
-# label2.grid(row=2, column=0)
-
-list_box = tk.Listbox(frame, bg="black",bd=0, font=("", 12, "bold"), justify="right" ,highlightthickness=0 , fg="white", width=50, height=(start.winfo_height()*3))
-list_box.grid(row=0, column=0, rowspan=3, padx=10)
+list_box.grid(row=0, column=0, rowspan=4, padx=10)
 
 root.mainloop()
